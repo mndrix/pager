33f4f3c9057c228eec786770b5051bce19454ad7
Michael Hendricks <michael@ndrix.org>
2015-01-05 16:25:08 -0700

    Populate pack title

--- pack.pl
+++ pack.pl
@@ -1,7 +1,9 @@
 name(pager).
-title('').
-version('0.0.1').
-download('https://github.com/mndrix/pager/archive/v0.0.1.zip').
+title('Send output to user\'s preferred pager').
+
+version('0.1.0').
+download('https://github.com/mndrix/pager/archive/v0.1.0.zip').
+
 author('Michael Hendricks','michael@ndrix.org').
 packager('Michael Hendricks','michael@ndrix.org').
 maintainer('Michael Hendricks','michael@ndrix.org').
